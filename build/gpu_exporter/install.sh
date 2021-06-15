46f1acf738369d7ba15cf3d231d9d8eb363c00bc
pHantomU94 <dzzxhushengjie@126.com>
2021-06-15 16:16:21 +0800

    fix install script

--- build/gpu_exporter/install.sh
+++ build/gpu_exporter/install.sh
@@ -7,10 +7,11 @@
  # @Author: hsj
  # @Date: 2021-06-15 11:17:25
  # @LastEditors: hsj
- # @LastEditTime: 2021-06-15 11:21:46
+ # @LastEditTime: 2021-06-15 16:15:42
 ### 
 
-sudo mv gpu_exporter /usr/bin/gpu_exporter
+sudo cp gpu_exporter /usr/bin/gpu_exporter
 sudo cp gpu_exporter.service /etc/systemd/system/gpu_exporter.service
+sudo systemctl systemctl daemon-reload
 sudo systemctl enable gpu_exporter.service
 sudo systemctl start gpu_exporter.service
