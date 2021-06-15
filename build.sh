de10661a8d18df39fb70e014086fd2fe739bb5ae
pHantomU94 <dzzxhushengjie@126.com>
2021-06-15 13:22:43 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@
  # @Author: hsj
  # @Date: 2021-06-15 11:10:15
  # @LastEditors: hsj
- # @LastEditTime: 2021-06-15 11:25:52
+ # @LastEditTime: 2021-06-15 13:18:17
 ### 
 
 if [ ! -d "./release" ]; then
   mkdir release
 fi
 
-go build . && cp gpu_exporter ./build/gpu_exporter/gpu_exporter && tar -czvf ./release/gpu_exporter.tar.gz -C ./build/ gpu_exporter/
+go build -o ./build/gpu_exporter/ . && tar -czvf ./release/gpu_exporter.tar.gz -C ./build/ gpu_exporter/
