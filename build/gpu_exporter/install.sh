02422ae4d8c10b6720794b1b534505bcd69077dc
pHantomU94 <dzzxhushengjie@126.com>
2021-06-15 13:22:02 +0800

    build files

---
+++ build/gpu_exporter/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+###
+ # @Title: gpu_exporter
+ # @Description:  gpu_exporter安装程序
+ # @Version: v1.0
+ # @Company: Casia
+ # @Author: hsj
+ # @Date: 2021-06-15 11:17:25
+ # @LastEditors: hsj
+ # @LastEditTime: 2021-06-15 11:21:46
+### 
+
+sudo mv gpu_exporter /usr/bin/gpu_exporter
+sudo cp gpu_exporter.service /etc/systemd/system/gpu_exporter.service
+sudo systemctl enable gpu_exporter.service
+sudo systemctl start gpu_exporter.service
